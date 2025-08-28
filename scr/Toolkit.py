73276e1b7bf218dc5d47f37b6d89dde22750d291
maximilien_notz <max@pleinchamps.ch>
2025-08-28 23:37:51 +0200

    fixing Find_DNA_Subsequences

--- scr/Toolkit.py
+++ scr/Toolkit.py
@@ -1,31 +1,5 @@
 from typing import List
 
-def Find_DNA_Subsequences(templateStrand: str, PromoterLst: List[str] = ["TATAAT", "TTGACA"], Terminator: str = "TTTTT") -> List[List[str]]:
-    output  = []
-    startIndexs = []
-    end = -1
-
-    for i in range(len(templateStrand)):
-        for promoter in PromoterLst:
-            if templateStrand[i:i + len(promoter)] == promoter:
-                startIndexs.append(i + len(promoter))
-                continue
-
-        if Terminator == templateStrand[i:i + len(Terminator)]:
-            end = i
-            break
-
-    # Error Handling
-    if end == -1:
-        raise Exception("[def Find_DNA_Subsequences(templateStrand: str):] No terminator found in templateStrand")
-    if len(startIndexs) == 0:
-        raise Exception("[def Find_DNA_Subsequences(templateStrand: str):] No promotor found in templateStrand")
-
-
-    for start in startIndexs:
-        output.append(templateStrand[start:end])
-    
-    return output
 
 def DNA_Subsequence_To_mRNA(dnaSubsequence: str) -> str:
 
@@ -44,5 +18,31 @@ def DNA_Subsequence_To_mRNA(dnaSubsequence: str) -> str:
 
         RNA_Sequence = RNA_Sequence + DNA_RNA_maping[b]
 
-
     return RNA_Sequence
+
+
+def Find_DNA_Subsequences(templateStrand: str, PromoterLst: List[str] = ["TATAAT", "TTGACA"], Terminator: str = "TTTTT") -> List[List[str]]:
+    dnaOutput  = []
+    promoterOutput = []
+    temp = []
+    startIndexs = []
+
+    i = 0
+    while i < len(templateStrand):
+        for promoter in PromoterLst:
+            if templateStrand[i:i + len(promoter)] == promoter:
+                startIndexs.append(i + len(promoter))
+                temp.append(promoter)##########################
+                continue
+
+        if Terminator == templateStrand[i:i + len(Terminator)]:
+            for start in startIndexs:
+                dnaOutput.append(templateStrand[start:i])
+                promoterOutput.append(temp.pop(0))
+            templateStrand = templateStrand[i + len(Terminator):]
+            print(templateStrand)
+            i = 0
+            startIndexs = []
+        i = i + 1
+    
+    return [dnaOutput, promoterOutput]
