86d252660b2d16186a614f05faae7b51294bd8ee
maximilien_notz <max@pleinchamps.ch>
2025-08-28 11:01:10 +0200

    implementing test cases

--- scr/tests.py
+++ scr/tests.py
@@ -1,10 +1,10 @@
 from Toolkit import *
 import pytest
+import yaml
+with open('scr/tests.yaml', 'r') as file: 
+    testsCases = yaml.safe_load(file)
 
-@pytest.mark.parametrize("dnaSubsequence, expected", [
-    ("TACGAT", "AUGCUA"),
-    ("AGCY", "-1"),
-    ])
+@pytest.mark.parametrize("dnaSubsequence, expected", testsCases["DNA_Subsequence_To_mRNA"])
 def test_DNA_Subsequence_To_mRNA(dnaSubsequence: str, expected: str) -> None:
     if expected == "-1":
         try:
@@ -12,7 +12,7 @@ def test_DNA_Subsequence_To_mRNA(dnaSubsequence: str, expected: str) -> None:
         except Exception:
             return
         else:
-            raise Exception("Missed cached error")
+            raise Exception("Uncached error")
         
     else:
         assert(DNA_Subsequence_To_mRNA(dnaSubsequence) == expected)
