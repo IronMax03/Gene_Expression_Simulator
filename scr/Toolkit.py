6f152ec8c1376969cc8a789427253f84caf84467
maximilien_notz <max@pleinchamps.ch>
2025-08-29 13:39:12 +0200

    add test case and small fix in DNA_Subsequence_To_mRNA

--- scr/Toolkit.py
+++ scr/Toolkit.py
@@ -13,10 +13,10 @@ def DNA_Subsequence_To_mRNA(dnaSubsequence: str) -> str:
     RNA_Sequence = ""
 
     for b in dnaSubsequence:
-        if b not in ['A', 'T', 'C', 'G']:
+        if b not in ['A', 'T', 'C', 'G', 'a', 't', 'c', 'g']:
             raise Exception("[def DNA_Subsequence_To_mRNA(dnaSubsequence: str):] Dna strand formating error.")
 
-        RNA_Sequence = RNA_Sequence + DNA_RNA_maping[b]
+        RNA_Sequence = RNA_Sequence + DNA_RNA_maping[b.upper()]
 
     return RNA_Sequence
 
@@ -32,17 +32,19 @@ def Find_DNA_Subsequences(templateStrand: str, PromoterLst: List[str] = ["TATAAT
         for promoter in PromoterLst:
             if templateStrand[i:i + len(promoter)] == promoter:
                 startIndexs.append(i + len(promoter))
-                temp.append(promoter)##########################
+                temp.append(promoter)
                 continue
 
         if Terminator == templateStrand[i:i + len(Terminator)]:
             for start in startIndexs:
                 dnaOutput.append(templateStrand[start:i])
                 promoterOutput.append(temp.pop(0))
+            temp.clear()
             templateStrand = templateStrand[i + len(Terminator):]
             print(templateStrand)
             i = 0
             startIndexs = []
+            continue
         i = i + 1
     
     return [dnaOutput, promoterOutput]
